72918e8ae3660cc25b91cbfc9aa566d1f9c458c7
samuelmalik <samuelmalik9@gmail.com>
2022-10-09 15:45:01 +0200

    added function returning string with repeated letters

--- Open-Lab-06.01/Challenge.cs
+++ Open-Lab-06.01/Challenge.cs
@@ -6,7 +6,17 @@ namespace Open_Lab_06._01
     {
         public string MysteryFunc(string str)
         {
-            throw new NotImplementedException();
+            string result = "";
+
+            for (int i = 0; i < str.Length; i = i+2)
+            {
+                    for (int x = 0; x < Int32.Parse(str[i+1].ToString()); x++)
+                    {
+                        result += str[i];
+                    }    
+            }
+
+            return result;
         }
     }
 }
